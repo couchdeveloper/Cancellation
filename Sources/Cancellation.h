c09523402cf507afebad0eec3bbce2383a198459
Andreas Grosam <agrosam@onlinehome.de>
2018-04-20 10:18:41 +0200

    Add umbrella header

---
+++ Sources/Cancellation.h
@@ -0,0 +1,15 @@
+//
+//  Cancellation.h
+//  Cancellation
+//
+//  Created by Andreas Grosam on 19.04.18.
+//  Copyright © 2018 Andreas Grosam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cancellation.
+FOUNDATION_EXPORT double CancellationVersionNumber;
+
+//! Project version string for Cancellation.
+FOUNDATION_EXPORT const unsigned char CancellationVersionString[];
